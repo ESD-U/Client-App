cd2cb192167f139baff35fde112950e2b6ce0ccc
kimkyung1234 <rter65341@gmail.com>
2021-10-27 22:50:28 +0900

    Change widget

--- lib/widgets/list_widget.dart
+++ lib/widgets/list_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:http/http.dart';
 
 class ListWidget extends StatelessWidget {
   @override
@@ -15,26 +16,43 @@ class ListWidget extends StatelessWidget {
         slivers: <Widget>[
           SliverStaggeredGrid.count(
             crossAxisCount: 4,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
+            mainAxisSpacing: 14,
+            crossAxisSpacing: 14,
             staggeredTiles: const <StaggeredTile>[
               StaggeredTile.count(2, 2.5),
               StaggeredTile.count(2, 1.8),
               StaggeredTile.count(2, 3.2),
               StaggeredTile.count(2, 1.8),
             ],
-            children: const <Widget>[
-              Card(
-                color: Color(0xffffffff), // Light
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  padding: const EdgeInsets.all(7),
+                  color: Color(0xffffffff),
+                  child: Column(),
+                ),
               ),
-              Card(
-                color: Color(0xffFB7B4A), // Temperature
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  padding: const EdgeInsets.all(7),
+                  color: Color(0xffFB7B4A),
+                ),
               ),
-              Card(
-                color: Color(0xff9D9D9D), // Streaming
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  padding: const EdgeInsets.all(7),
+                  color: Color(0xff9D9D9D),
+                ),
               ),
-              Card(
-                color: Color(0xff6D7DD2), // Humidity
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  padding: const EdgeInsets.all(7),
+                  color: Color(0xff6D7DD2),
+                ),
               ),
             ],
           ),
