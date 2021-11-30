2ad255da30337f5669a4f2b5d60130687f822029
kimkyung1234 <rter65341@gmail.com>
2021-11-30 12:41:48 +0900

    Change to widget

--- lib/widgets/list_widget.dart
+++ lib/widgets/list_widget.dart
@@ -1,139 +1,137 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:http/http.dart';
 import 'package:sliding_switch/sliding_switch.dart';
 
-class ListWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 22,
-        right: 22,
-        top: 22,
-      ),
-      child: CustomScrollView(
-        primary: false,
-        slivers: <Widget>[
-          SliverStaggeredGrid.count(
-            crossAxisCount: 4,
-            mainAxisSpacing: 12,
-            crossAxisSpacing: 12,
-            staggeredTiles: const <StaggeredTile>[
-              StaggeredTile.count(2, 2.7),
-              StaggeredTile.count(2, 2),
-              StaggeredTile.count(2, 3.2),
-              StaggeredTile.count(2, 2),
-            ],
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Container(
-                  padding: const EdgeInsets.all(7),
-                  color: Color(0xffffffff),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 10),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          '   ESD LED',
-                          style: TextStyle(
-                            fontFamily: 'sen',
-                          ),
+Widget ListWidget(double temp, double humi) {
+  return Padding(
+    padding: const EdgeInsets.only(
+      left: 22,
+      right: 22,
+      top: 22,
+    ),
+    child: CustomScrollView(
+      primary: false,
+      slivers: <Widget>[
+        SliverStaggeredGrid.count(
+          crossAxisCount: 4,
+          mainAxisSpacing: 12,
+          crossAxisSpacing: 12,
+          staggeredTiles: const <StaggeredTile>[
+            StaggeredTile.count(2, 2.7),
+            StaggeredTile.count(2, 2),
+            StaggeredTile.count(2, 3.2),
+            StaggeredTile.count(2, 2),
+          ],
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                padding: const EdgeInsets.all(7),
+                color: Color(0xffffffff),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        '   ESD LED',
+                        style: TextStyle(
+                          fontFamily: 'sen',
                         ),
                       ),
-                      SizedBox(height: 20),
-                      Center(
-                        child: Icon(
-                          Icons.wb_incandescent,
-                          size: 80,
-                          color: Colors.grey,
-                        ),
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: Icon(
+                        Icons.wb_incandescent,
+                        size: 80,
+                        color: Colors.grey,
                       ),
-                      SizedBox(height: 10),
-                      SlidingSwitch(
-                        value: false,
-                        width: 130,
-                        onChanged: (bool value) {
-                          print(value);
-                        },
-                      )
-                    ],
-                  ),
+                    ),
+                    SizedBox(height: 10),
+                    SlidingSwitch(
+                      value: false,
+                      width: 130,
+                      onChanged: (bool value) {
+                        print(value);
+                      },
+                    )
+                  ],
                 ),
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Container(
-                  padding: const EdgeInsets.all(7),
-                  color: Color(0xffFB7B4A),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 10),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          '  Temperature',
-                          style:
-                              TextStyle(fontFamily: 'sen', color: Colors.white),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                padding: const EdgeInsets.all(7),
+                color: Color(0xffFB7B4A),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        '  Temperature',
+                        style:
+                            TextStyle(fontFamily: 'sen', color: Colors.white),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        //'20',
+                        temp.toString(),
+                        style: TextStyle(
+                          fontFamily: 'sen',
+                          color: Colors.white,
+                          fontSize: 70,
                         ),
                       ),
-                      Center(
-                        child: Text(
-                          '20',
-                          style: TextStyle(
-                            fontFamily: 'sen',
-                            color: Colors.white,
-                            fontSize: 70,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Container(
-                  padding: const EdgeInsets.all(7),
-                  color: Color(0xff9D9D9D),
-                ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                padding: const EdgeInsets.all(7),
+                color: Color(0xff9D9D9D),
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Container(
-                  padding: const EdgeInsets.all(7),
-                  color: Color(0xff6D7DD2),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 10),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          '  Humidity',
-                          style:
-                              TextStyle(fontFamily: 'sen', color: Colors.white),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                padding: const EdgeInsets.all(7),
+                color: Color(0xff6D7DD2),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        '  Humidity',
+                        style:
+                            TextStyle(fontFamily: 'sen', color: Colors.white),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        //'20',
+                        humi.toString(),
+                        style: TextStyle(
+                          fontFamily: 'sen',
+                          color: Colors.white,
+                          fontSize: 70,
                         ),
                       ),
-                      Center(
-                        child: Text(
-                          '20',
-                          style: TextStyle(
-                            fontFamily: 'sen',
-                            color: Colors.white,
-                            fontSize: 70,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
